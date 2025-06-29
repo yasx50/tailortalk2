acdc4947a50facbce65ef8041afa298f836dca01
yasx50 <yasxcode@gmail.com>
2025-06-29 19:52:16 +0530

    PORT ISSUE

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from ai_agent.ai_integration import compiled_graph
 from ai_agent.fetch_calendar import get_all_calendars
 from ai_agent.events import get_all_events
+import uvicorn
+import os
 
 
 
@@ -30,3 +32,8 @@ def fetch_calendar():
 @app.get("/events")
 def fetch_calendar():
     return get_all_events()
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Use PORT env variable or fallback to 8000
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
